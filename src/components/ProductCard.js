132b6b79ffdbfc309406e4447215107aa0316758
dsukanta <sukanta@semicolonites.com>
2023-11-01 18:51:58 +0530

    product design updated

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
-import { globalStyles } from '../constants/globalStyles';
-import { Button } from '@rneui/base';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import {globalStyles} from '../constants/globalStyles';
+import {Button} from '@rneui/base';
+import {Rating, AirbnbRating} from 'react-native-ratings';
 
-const ProductCard = ({ data }) => {
+const ProductCard = ({data}) => {
   return (
     <View style={styles.card}>
       <View style={styles.leftSide}>
         <Text style={styles.title}>{data?.name}</Text>
         <View style={styles.ratingContainer}>
-          <Text style={styles.rating}>{4.5}</Text>
-          <Text style={styles.ratingCount}>({96})</Text>
+          <View
+            style={{
+              padding: 5,
+              borderWidth: 1,
+              borderColor: '#f1c40e',
+              borderRadius: 5,
+            }}>
+            <Rating
+              type="star"
+              ratingCount={5}
+              imageSize={10}
+              readonly
+              startingValue={4.5}
+            />
+          </View>
+          {/* <Text style={styles.rating}>{4.5}</Text> */}
+          <Text style={styles.ratingCount}>{96} ratings</Text>
         </View>
-        <Text style={[globalStyles.text,{color:'black'}]}>${299}</Text>
-        <Text style={[globalStyles.text,{color:'grey'}]}>{'details'}</Text>
+        <Text style={[globalStyles.text, {color: 'black'}]}>${299}</Text>
+        <Text
+          style={styles.detailsText}>{(`Lorem Ipsum is simply dummy text of the printing and typesetting industry.`).substring(0,60)}<Text style={[styles.detailsText,{color:'black',fontWeight:'600'}]}>...read more</Text></Text>
       </View>
       <View style={styles.rightSide}>
         <Image source={data?.image} style={styles.image} />
         <View style={styles.buttonContainer}>
-          <Button title="Add" buttonStyle={{backgroundColor:'red'}}/>
+          <Button title="Add" buttonStyle={{backgroundColor: 'red'}} />
         </View>
       </View>
     </View>
@@ -44,7 +61,7 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 18,
     fontWeight: 'bold',
-    color:'black'
+    color: 'black',
   },
   ratingContainer: {
     flexDirection: 'row',
@@ -55,8 +72,9 @@ const styles = StyleSheet.create({
     marginRight: 5,
   },
   ratingCount: {
-    fontSize: 14,
-    color: 'gray',
+    fontSize: 12,
+    color: 'black',
+    margin: 10,
   },
   image: {
     width: '100%',
@@ -72,6 +90,11 @@ const styles = StyleSheet.create({
     width: '50%',
     backgroundColor: 'white',
   },
+  detailsText:{
+    fontSize:13,
+    fontWeight:'400',
+    color:'grey',
+  }
 });
 
 export default ProductCard;
