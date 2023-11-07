1a3965f0e8535259253fbe10c8b06382f57be9f2
dsukanta <sukanta@semicolonites.com>
2023-11-07 13:02:37 +0530

    cart design updated

--- src/screens/Cart.js
+++ src/screens/Cart.js
@@ -1,4 +1,11 @@
-import {ScrollView, StyleSheet, Text, View, Dimensions} from 'react-native';
+import {
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  Image,
+} from 'react-native';
 import React, {useContext} from 'react';
 import RedLine from '../components/RedLine';
 import {globalStyles} from '../constants/globalStyles';
@@ -9,8 +16,8 @@ import colors from '../constants/colors';
 
 const {height, width} = Dimensions.get('window');
 
-const Cart = ({navigation,route}) => {
-  const {userCart,cartTotal} = useContext(Appcontext);
+const Cart = ({navigation, route}) => {
+  const {userCart, cartTotal} = useContext(Appcontext);
 
   // const getCartTotal = () => {
   //   const total = userCart.reduce(
@@ -21,41 +28,39 @@ const Cart = ({navigation,route}) => {
   // };
 
   return (
-      <ScrollView
-        style={styles.container}
-        showsVerticalScrollIndicator={false}>
+    <ScrollView style={styles.container} showsVerticalScrollIndicator={false}>
+      <View>
+        <RedLine text={'your cart'} />
+      </View>
+      {userCart.length ? (
         <View>
-          <RedLine text={'your cart'} />
-        </View>
-        {userCart.length ? (
-          <View>
-            <Text style={[globalStyles.text,{alignSelf:'center',margin:10}]}>Ites(s) Added</Text>
-            {userCart.map((cart, i) => (
-              <CartCard data={cart} key={i} />
-            ))}
-            <Button
-              title={'+ Add More'}
-              buttonStyle={{
-                backgroundColor: 'transparent',
-                borderWidth: 1,
-                borderColor: colors.red,
-                borderRadius: 10,
-                marginVertical: 5,
-              }}
-              containerStyle={{width:'90%',alignSelf: 'center'}}
-              titleStyle={globalStyles.text}
-              onPress={()=>navigation.navigate('Menu')}
-            />
-            <View style={styles.rowContainer}>
+          <Text style={[globalStyles.text, {alignSelf: 'center', margin: 10}]}>
+            Ites(s) Added
+          </Text>
+          {userCart.map((cart, i) => (
+            <CartCard data={cart} key={i} />
+          ))}
+          <Button
+            title={'+ Add More'}
+            buttonStyle={{
+              backgroundColor: 'transparent',
+              borderWidth: 1,
+              borderColor: colors.red,
+              borderRadius: 10,
+              marginVertical: 5,
+            }}
+            containerStyle={{width: '90%', alignSelf: 'center'}}
+            titleStyle={globalStyles.text}
+            onPress={() => navigation.navigate('Menu')}
+          />
+          <View style={styles.rowContainer}>
             <View style={styles.row}>
               <Text style={[globalStyles.text]}>Total Price :</Text>
               <Text style={[globalStyles.text]}>{cartTotal}</Text>
             </View>
             <View style={styles.row}>
               <Text style={[globalStyles.text]}>GST :</Text>
-              <Text style={[globalStyles.text]}>
-                {(cartTotal * 18) / 100}
-              </Text>
+              <Text style={[globalStyles.text]}>{(cartTotal * 18) / 100}</Text>
             </View>
             <View style={styles.row}>
               <Text style={[globalStyles.text]}>Grand Total :</Text>
@@ -63,25 +68,43 @@ const Cart = ({navigation,route}) => {
                 {cartTotal + (cartTotal * 18) / 100}
               </Text>
             </View>
-            </View>
-            <Button
-              title={'Proceed to Checkout'}
-              buttonStyle={{
-                padding: 10,
-                backgroundColor: colors.red,
-                borderWidth: 1,
-                borderColor: colors.red,
-              }}
-              containerStyle={{width:'90%',borderRadius:10,marginBottom:'18%',alignSelf: 'center'}}
-            />
           </View>
-        ) : (
-          <View
-            style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text style={globalStyles.text}>No Items</Text>
-          </View>
-        )}
-      </ScrollView>
+          <Button
+            title={'Proceed to Checkout'}
+            buttonStyle={{
+              padding: 10,
+              backgroundColor: colors.red,
+              borderWidth: 1,
+              borderColor: colors.red,
+            }}
+            containerStyle={{
+              width: '90%',
+              borderRadius: 10,
+              marginBottom: '18%',
+              alignSelf: 'center',
+            }}
+          />
+        </View>
+      ) : (
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+          <Image
+            source={require('../images/empty-cart.jpg')}
+            style={{
+              width: 300,
+              height: 300,
+              resizeMode: 'contain',
+              borderRadius: 10,
+            }}
+          />
+          <Button
+            title={'Explore Menu'}
+            onPress={() => navigation.navigate('Menu')}
+            buttonStyle={{paddingVertical:10,borderRadius:10,backgroundColor:colors.red}}
+            containerStyle={{width: '90%', alignSelf: 'center',marginVertical:15}}
+          />
+        </View>
+      )}
+    </ScrollView>
   );
 };
 
@@ -101,8 +124,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
   },
-  rowContainer:{
+  rowContainer: {
     margin: 10,
     padding: 10,
-  }
+  },
 });
