24d9543ae8b3d6f48345345f49151f4eae67b52a
dSukanta <130562477+dSukanta@users.noreply.github.com>
2023-11-14 11:19:50 +0530

    Updated

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,46 +1,42 @@
-import {View, Text, ScrollView, StyleSheet, Image, Dimensions, FlatList} from 'react-native';
+import {
+  View,
+  Text,
+  ScrollView,
+  StyleSheet,
+  Image,
+  Dimensions,
+  FlatList,
+} from 'react-native';
 import React, {useState} from 'react';
 import {Button, ListItem} from '@rneui/base';
-import {globalStyles} from '../constants/globalStyles';
 import Feather from 'react-native-vector-icons/Feather';
 import FontAwesome6 from 'react-native-vector-icons/FontAwesome6';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
-import colors from '../constants/colors';
 
 const {height, width} = Dimensions.get('window');
 
-
-const Profile = ({navigation}) => {
+const Landing = ({navigation}) => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
-
-  const handlenavigate= async(path)=>{
-      if(isLoggedIn || path === 'Help'){
-        navigation.navigate(path);
-      }else{
-        navigation.navigate('Auth')
-      }
-  };
-
-  const listItems=[
+  const listItems = [
     {
-      title:'Settings & Personalization',
-      icon: <MaterialIcons name='settings' size={30} color={'white'}/>,
-      onPress:()=> handlenavigate('Settings')
+      title: 'Settings & Personalization',
+      icon: <MaterialIcons name="settings" size={30} color={'white'} />,
+      onPress: () => handlenavigate('Settings'),
     },
     {
-      title:'Addresses',
-      icon: <MaterialIcons name='add-home-work' size={30} color={'white'}/>,
-      onPress:()=> handlenavigate('Addresses')
+      title: 'Addresses',
+      icon: <MaterialIcons name="add-home-work" size={30} color={'white'} />,
+      onPress: () => handlenavigate('Addresses'),
     },
     {
-      title:'Help & Support',
-      icon: <MaterialIcons name='support-agent' size={30} color={'white'}/>,
-      onPress:()=> handlenavigate('Help')
+      title: 'Help & Support',
+      icon: <MaterialIcons name="support-agent" size={30} color={'white'} />,
+      onPress: () => handlenavigate('Help'),
     },
     {
-      title:'Passwords',
-      icon: <MaterialIcons name='fingerprint' size={30} color={'white'}/>,
-      onPress:()=> handlenavigate('Passwords')
+      title: 'Passwords',
+      icon: <MaterialIcons name="fingerprint" size={30} color={'white'} />,
+      onPress: () => handlenavigate('Passwords'),
     },
     // {
     //   title:'Video Engagement and Rewards',
@@ -48,24 +44,22 @@ const Profile = ({navigation}) => {
     //   onPress:()=> handlenavigate('FoodReels')
     // },
     {
-      title:'Orders',
+      title: 'Orders',
       icon: <Feather name="truck" size={30} color={'white'} />,
-      onPress:()=> handlenavigate('Orders')
+      onPress: () => handlenavigate('Orders'),
     },
   ];
-
-  const renderRow = ({ item }) => {
+  const renderRow = ({item}) => {
     return (
       <ListItem
         onPress={item.onPress}
         bottomDivider
         containerStyle={{
-          backgroundColor:'transparent',
-          padding:0,
-          margin:0,
-          paddingVertical:15
-        }}
-      >
+          backgroundColor: 'transparent',
+          padding: 0,
+          margin: 0,
+          paddingVertical: 15,
+        }}>
         {item.icon}
         <ListItem.Content>
           <ListItem.Title style={[globalStyles.text]}>{item.title}</ListItem.Title>
@@ -75,36 +69,40 @@ const Profile = ({navigation}) => {
     );
   };
 
-
-
   return (
     <ScrollView style={styles.container}>
-      {!isLoggedIn && (
+      {/* {!isLoggedIn && (
         <View>
-          <View style={{flexDirection:'row',justifyContent:'space-between',alignItems:'center',marginTop:15,marginHorizontal:20}}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              marginTop: 15,
+              marginHorizontal: 20,
+            }}>
             <View>
-              <Text style={[globalStyles.text, {fontSize: 18}]}>
-                Your Account
-              </Text>
-              <Text style={[globalStyles.text, {fontWeight: '400'}]}>
-                Login to manage your account.
-              </Text>
+              <Text>Your Account</Text>
+              <Text>Login to manage your account.</Text>
             </View>
             <View>
               <Button
                 title="Login"
                 onPress={() => navigation.navigate('Auth')}
-                color={colors.red}
-                buttonStyle={{paddingHorizontal:width/9,borderRadius:10}}
-                containerStyle={{justifyContent: 'center',alignItems: 'center'}}
+                // color={colors.red}
+                buttonStyle={{paddingHorizontal: width / 9, borderRadius: 10}}
+                containerStyle={{
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}
               />
             </View>
           </View>
-          <View style={{margin:20,marginTop:30}}>
+          <View style={{margin: 20, marginTop: 30}}>
             <FlatList
-            data={listItems}
-            keyExtractor={(index) => index.toString()}
-            renderItem={renderRow}
+              data={listItems}
+              keyExtractor={index => index.toString()}
+              renderItem={renderRow}
             />
           </View>
         </View>
@@ -121,7 +119,9 @@ const Profile = ({navigation}) => {
             <View
               style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
               <Image
-                source={require('../images/user.jpg')}
+                source={{
+                  uri: 'https://cdn-icons-png.flaticon.com/512/3177/3177440.png',
+                }}
                 style={styles.profileimgStyle}
               />
             </View>
@@ -145,29 +145,78 @@ const Profile = ({navigation}) => {
               }}>
               {'User Name'}
             </Text>
-            <Text
-              style={[globalStyles.text, {color: 'red', marginHorizontal: 10}]}>
-              {'user@email'}
-            </Text>
+            <Text>{'user@email'}</Text>
           </View>
-          <View style={{margin:20,marginTop:30}}>
+          <View style={{margin: 20, marginTop: 30}}>
             <FlatList
-            data={listItems}
-            keyExtractor={(index) => index.toString()}
-            renderItem={renderRow}
+              data={listItems}
+              keyExtractor={index => index.toString()}
+              renderItem={renderRow}
             />
           </View>
         </View>
+      )} */}
+      {!isLoggedIn ? (
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            marginTop: 15,
+            marginHorizontal: 20,
+          }}>
+          <View>
+            <Text style={[globalStyles.text, {fontSize: 18}]}>Your Account</Text>
+            <Text style={[globalStyles.text, {fontWeight: '400'}]}>Login to manage your account.</Text>
+          </View>
+          <View>
+            <Button
+              title="Login"
+              onPress={() => navigation.navigate('Auth')}
+              color={colors.red}
+              buttonStyle={{paddingHorizontal: width / 9, borderRadius: 10}}
+              containerStyle={{justifyContent: 'center', alignItems: 'center'}}
+            />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.headerContainer}>
+          <View
+            style={{
+              flexDirection: 'row',
+              gap: 10,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <MaterialIcons name="person" size={25} />
+            <Text style={[globalStyles.text, {fontSize: 18}]}>Sukanta</Text>
+          </View>
+          <MaterialIcons name="emoji-events" size={25} />
+        </View>
       )}
+      <View style={{margin: 20, marginTop: 30}}>
+        <FlatList
+          data={listItems}
+          keyExtractor={index => index.toString()}
+          renderItem={renderRow}
+        />
+      </View>
     </ScrollView>
   );
 };
 
-export default Profile;
+export default Landing;
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'black',
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 10,
+    padding: 10,
+    alignItems: 'center',
   },
   profileimgStyle: {
     width: 100,
