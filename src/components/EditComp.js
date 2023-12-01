8b2e1576a23b492cd0616cd07acf6fe8718d2ab6
dsukanta <sukanta@semicolonites.com>
2023-12-01 18:04:39 +0530

    profile page updated

--- src/components/EditComp.js
+++ src/components/EditComp.js
@@ -6,6 +6,8 @@ import {
   TextInput,
   ScrollView,
   TouchableOpacity,
+  ImageBackground,
+  Dimensions,
 } from 'react-native';
 import React, {useContext, useEffect, useState} from 'react';
 import {Input, ListItem} from '@rneui/themed';
@@ -17,6 +19,8 @@ import {Appcontext} from '../context/AppContext';
 import {getProfileName} from '../utils/Helper';
 import DatePicker from 'react-native-date-picker';
 import moment from 'moment';
+
+const {height, width} = Dimensions.get('window');
 
 const EditComp = ({route, navigation}) => {
   const [edit, setEdit] = useState(false);
@@ -39,12 +43,32 @@ const EditComp = ({route, navigation}) => {
 
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
+       <ImageBackground
+        source={{uri: 'https://shorturl.at/LXZ16'}}
+        style={styles.upperContainer}
+        imageStyle={{opacity: 0.5}}>
+      </ImageBackground>
+      <View style={styles.imageContainer}>
+        <Image
+          source={{
+            uri: 'https://shorturl.at/LXZ16',
+          }}
+          style={styles.image}
+        />
+        <Text style={[globalStyles.text, {marginBottom: 10, fontSize: 18}]}>
+          Name
+        </Text>
+      </View>
+      <View style={{marginTop:'23%'}}>
       <Input
         placeholder="Enter your name"
         leftIcon={
           <MaterialCommunityIcons name="account" size={24} color="white" />
         }
         inputContainerStyle={styles.inputStyle}
+        inputStyle={{color:'white'}}
+        value={inputData?.fullname}
+        onChangeText={(text)=>setInputData({...inputData, fullname: text})}
       />
       <Input
         placeholder="Enter your email"
@@ -52,11 +76,25 @@ const EditComp = ({route, navigation}) => {
           <MaterialCommunityIcons name="email" size={24} color="white" />
         }
         inputContainerStyle={styles.inputStyle}
+        inputStyle={{color:'white'}}
+        value={inputData?.email}
+        onChangeText={(text)=>setInputData({...inputData, email: text})}
+        disabled={inputData?.loginMethod==='email'}
       />
-     <TouchableOpacity style={styles.dateContainer} onPress={()=>setOpen(true)}>
-        <MaterialCommunityIcons name='calendar-account' size={24} color="white" />
-        <Text style={globalStyles.text}>{inputData?.dob? moment(inputData?.dob)?.format('ll') : 'Select your date of birth'}</Text>
-     </TouchableOpacity>
+      <TouchableOpacity
+        style={styles.dateContainer}
+        onPress={() => setOpen(true)}>
+        <MaterialCommunityIcons
+          name="calendar-account"
+          size={24}
+          color="white"
+        />
+        <Text style={globalStyles.text}>
+          {inputData?.dob
+            ? moment(inputData?.dob)?.format('ll')
+            : 'Select your date of birth'}
+        </Text>
+      </TouchableOpacity>
       <DatePicker
         modal
         open={open}
@@ -64,7 +102,7 @@ const EditComp = ({route, navigation}) => {
         onConfirm={date => {
           setOpen(false);
           setDate(date);
-          setInputData({...inputData, dob:date});
+          setInputData({...inputData, dob: date});
         }}
         onCancel={() => {
           setOpen(false);
@@ -78,81 +116,12 @@ const EditComp = ({route, navigation}) => {
           <MaterialCommunityIcons name="cellphone" size={24} color="white" />
         }
         inputContainerStyle={styles.inputStyle}
+        inputStyle={{color:'white'}}
+        value={inputData?.phone}
+        onChangeText={(text)=>setInputData({...inputData, phone: text})}
+        disabled={inputData?.loginMethod==='phone'}
       />
-
-      {/* <ListItem bottomDivider containerStyle={styles.contentContainer}>
-        <ListItem.Content>
-          <View
-            style={{
-              flexDirection: 'row',
-              gap: 10,
-              justifyContent: 'center',
-              alignItems: 'center',
-            }}>
-            <MaterialCommunityIcons name="account" size={25} />
-            <ListItem.Subtitle>Name</ListItem.Subtitle>
-          </View>
-          <TextInput
-            placeholder="name"
-            style={styles.inputStyle}
-            onChangeText={text => setInputData({...inputData, fullname: text})}
-            value={inputData.fullname}
-            placeholderTextColor={'black'}
-          />
-        </ListItem.Content>
-      </ListItem>
-      <ListItem bottomDivider containerStyle={styles.contentContainer}>
-        <ListItem.Content>
-          <ListItem.Subtitle>Email</ListItem.Subtitle>
-
-          <TextInput
-            placeholder="email"
-            style={styles.inputStyle}
-            onChangeText={text => setInputData({...inputData, email: text})}
-            value={inputData.email}
-            placeholderTextColor={'black'}
-          />
-        </ListItem.Content>
-      </ListItem>
-      <ListItem bottomDivider containerStyle={styles.contentContainer}>
-        <ListItem.Content>
-          <ListItem.Subtitle>Date of Birth</ListItem.Subtitle>
-
-          <TextInput
-            placeholder="Date of Birth"
-            style={styles.inputStyle}
-            value={inputData.dob || moment(date).format('ll')}
-            placeholderTextColor={'black'}
-            onPressIn={() => setOpen(true)}
-          />
-          <DatePicker
-            modal
-            open={open}
-            date={date}
-            onConfirm={date => {
-              setOpen(false);
-              setDate(date);
-            }}
-            onCancel={() => {
-              setOpen(false);
-            }}
-            mode="date"
-            maximumDate={new Date('2022-12-31')}
-          />
-        </ListItem.Content>
-      </ListItem>
-      <ListItem bottomDivider containerStyle={styles.contentContainer}>
-        <ListItem.Content>
-          <ListItem.Subtitle>Phone no.</ListItem.Subtitle>
-          <TextInput
-            placeholder="Phone no."
-            style={styles.inputStyle}
-            onChangeText={text => setInputData({...inputData, phone: text})}
-            value={inputData.phone}
-            placeholderTextColor={'black'}
-          />
-        </ListItem.Content>
-      </ListItem> */}
+      </View>
     </ScrollView>
   );
 };
@@ -164,25 +133,47 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderRadius: 5,
     borderColor: 'white',
-    paddingHorizontal:15,
+    paddingHorizontal: 15,
   },
-  dateContainer:{
-    flexDirection:'row',
-    gap:15,
+  dateContainer: {
+    flexDirection: 'row',
+    gap: 15,
     alignItems: 'center',
-    borderWidth:1,
-    borderColor:'white',
-    padding:15,
-    width:'95%',
+    borderWidth: 1,
+    borderColor: 'white',
+    padding: 15,
+    width: '95%',
     alignSelf: 'center',
-    borderRadius:5,
-    marginBottom:'5%'
-  }
+    borderRadius: 5,
+    marginBottom: '5%',
+  },
+  upperContainer: {
+    height: height / 3.5,
+    backgroundColor: 'transparent',
+    width: width,
+    padding: 0,
+    margin: 0,
+    borderWidth: 0,
+    position: 'relative',
+  },
+  imageContainer: {
+    width: width / 4,
+    height: width / 4,
+    borderRadius: width / 4,
+    alignSelf: 'center',
+    position: 'absolute',
+    top: width / 2.5,
+    left: width / 2.6,
+    zIndex: 1000,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    marginVertical: 5,
+    borderWidth: 5,
+    borderColor: 'white',
+  },
 });
-
-
-//     height: height / 2.5
-
-
-
-
